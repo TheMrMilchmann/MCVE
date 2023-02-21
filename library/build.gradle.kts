598e9ad8a057aac94866a35cf26237b5a39116a4
Leon Linhart <themrmilchmann@gmail.com>
2023-02-21 01:51:57 +0100

    Attempt to configure substitution for all configurations

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -3,6 +3,12 @@ plugins {
     `maven-publish`
 }
 
+configurations.configureEach {
+    resolutionStrategy.dependencySubstitution {
+        substitute(module("com.example:library")).using(project(":library"))
+    }
+}
+
 dependencies {
     testImplementation("com.example:sandbox:0.1.0")
 }
